5639f08d2a200a529eca1603c5852e18bdffbb19
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-01-11 22:30:04 +0100

    Switch to cowboy

--- apps/paperless/src/paperless_app.erl
+++ apps/paperless/src/paperless_app.erl
@@ -1,16 +1,24 @@
 -module(paperless_app).
-
 -behaviour(application).
+-export([start/2, stop/1, start/0]).
 
-%% Application callbacks
--export([start/2, stop/1]).
-
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
+start() ->
+    start_app(paperless, permanent).
 
 start(_StartType, _StartArgs) ->
     paperless_sup:start_link().
 
 stop(_State) ->
     ok.
+
+start_app(App, Type) ->
+    start_app(App, Type, application:start(App, Type)).
+start_app(App, Type, Status) ->
+    case Status of
+        ok             -> ok;
+        {error, Error} -> handle_error(Error, App, Type)
+    end.
+handle_error({already_started, _}, _App, _Type) -> ok;
+handle_error({not_started, Dep}, App, Type) ->
+    ok = start_app(Dep, Type),
+    ok = start_app(App, Type).
--- apps/paperless/src/paperless_sup.erl
+++ apps/paperless/src/paperless_sup.erl
@@ -10,7 +10,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -25,7 +25,7 @@ start_link() ->
 
 init([]) ->
     {ok, { {one_for_one, 5, 10}, [
-      ?CHILD(scanner_server, worker),
-      ?CHILD(http_server, worker)
+      ?CHILD(scanner_server, worker, []),
+      ?CHILD(http_server, worker, [8080])
     ]} }.
 
